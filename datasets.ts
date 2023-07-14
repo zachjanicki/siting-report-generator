e217bc75e7bb3c212d67c37e442703f1b25a85a4
zachjanicki <zjanicki88@gmail.com>
2023-07-13 18:01:18 -0600

    update enums and coalesce feature finding into a couple functions

---
+++ datasets.ts
@@ -0,0 +1,32 @@
+export enum GeoJsonDataset {
+    DisadvantagedCommunity,
+    ElectricLoadServingEntity,
+    CaAirDistrict,
+    CleanCitiesCoalition,
+    CaCounties,
+    CaAssemblyDistrict,
+    CaCongressionalLegislativeDistrict,
+    CaSenateLegislativeDistrict
+}
+
+export const PROJECTIONS = {
+    [GeoJsonDataset.DisadvantagedCommunity]: 'PROJCS["WGS 84 / Pseudo-Mercator",GEOGCS["WGS 84",DATUM["WGS_1984",SPHEROID["WGS 84",6378137,298.257223563,AUTHORITY["EPSG","7030"]],AUTHORITY["EPSG","6326"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.0174532925199433,AUTHORITY["EPSG","9122"]],AXIS["Latitude",NORTH],AXIS["Longitude",EAST],AUTHORITY["EPSG","4326"]],GeoJsonDataset["Mercator_1SP"],PARAMETER["central_meridian",0],PARAMETER["scale_factor",1],PARAMETER["false_easting",0],PARAMETER["false_northing",0],UNIT["metre",1,AUTHORITY["EPSG","9001"]],AXIS["X",EAST],AXIS["Y",NORTH],EXTENSION["PROJ4","+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs"],AUTHORITY["EPSG","3857"]]',
+    [GeoJsonDataset.ElectricLoadServingEntity]: 'PROJCS["WGS_1984_Web_Mercator_Auxiliary_Sphere",GEOGCS["GCS_WGS_1984",DATUM["D_WGS_1984",SPHEROID["WGS_1984",6378137.0,298.257223563]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]],PROJECTION["Mercator_Auxiliary_Sphere"],PARAMETER["False_Easting",0.0],PARAMETER["False_Northing",0.0],PARAMETER["Central_Meridian",0.0],PARAMETER["Standard_Parallel_1",0.0],PARAMETER["Auxiliary_Sphere_Type",0.0],UNIT["Meter",1.0]]',
+    [GeoJsonDataset.CaAirDistrict]: 'GEOGCS["GCS_WGS_1984",DATUM["D_WGS_1984",SPHEROID["WGS_1984",6378137.0,298.257223563]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]]',
+    [GeoJsonDataset.CleanCitiesCoalition]: 'GEOGCS["GCS_North_American_1983",DATUM["D_North_American_1983",SPHEROID["GRS_1980",6378137.0,298.257222101]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]]',
+    [GeoJsonDataset.CaCounties]: 'PROJCS["WGS_1984_Web_Mercator_Auxiliary_Sphere",GEOGCS["GCS_WGS_1984",DATUM["D_WGS_1984",SPHEROID["WGS_1984",6378137.0,298.257223563]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]],PROJECTION["Mercator_Auxiliary_Sphere"],PARAMETER["False_Easting",0.0],PARAMETER["False_Northing",0.0],PARAMETER["Central_Meridian",0.0],PARAMETER["Standard_Parallel_1",0.0],PARAMETER["Auxiliary_Sphere_Type",0.0],UNIT["Meter",1.0]]',
+    [GeoJsonDataset.CaAssemblyDistrict]: 'PROJCS["WGS_1984_Web_Mercator_Auxiliary_Sphere",GEOGCS["GCS_WGS_1984",DATUM["D_WGS_1984",SPHEROID["WGS_1984",6378137.0,298.257223563]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]],PROJECTION["Mercator_Auxiliary_Sphere"],PARAMETER["False_Easting",0.0],PARAMETER["False_Northing",0.0],PARAMETER["Central_Meridian",0.0],PARAMETER["Standard_Parallel_1",0.0],PARAMETER["Auxiliary_Sphere_Type",0.0],UNIT["Meter",1.0]]',
+    [GeoJsonDataset.CaCongressionalLegislativeDistrict]: 'PROJCS["WGS_1984_Web_Mercator_Auxiliary_Sphere",GEOGCS["GCS_WGS_1984",DATUM["D_WGS_1984",SPHEROID["WGS_1984",6378137.0,298.257223563]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]],PROJECTION["Mercator_Auxiliary_Sphere"],PARAMETER["False_Easting",0.0],PARAMETER["False_Northing",0.0],PARAMETER["Central_Meridian",0.0],PARAMETER["Standard_Parallel_1",0.0],PARAMETER["Auxiliary_Sphere_Type",0.0],UNIT["Meter",1.0]]',
+    [GeoJsonDataset.CaSenateLegislativeDistrict]: 'PROJCS["WGS_1984_Web_Mercator_Auxiliary_Sphere",GEOGCS["GCS_WGS_1984",DATUM["D_WGS_1984",SPHEROID["WGS_1984",6378137.0,298.257223563]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]],PROJECTION["Mercator_Auxiliary_Sphere"],PARAMETER["False_Easting",0.0],PARAMETER["False_Northing",0.0],PARAMETER["Central_Meridian",0.0],PARAMETER["Standard_Parallel_1",0.0],PARAMETER["Auxiliary_Sphere_Type",0.0],UNIT["Meter",1.0]]'
+}
+
+export const GEOJSON_FILES = {
+    [GeoJsonDataset.DisadvantagedCommunity]: 'geojson/dac.json',
+    [GeoJsonDataset.ElectricLoadServingEntity]: 'geojson/electric_load_serving_entities.json',
+    [GeoJsonDataset.CaAirDistrict]: 'geojson/ca_air_districts.json',
+    [GeoJsonDataset.CleanCitiesCoalition]: 'geojson/clean_cities_coalition.json',
+    [GeoJsonDataset.CaCounties]: 'geojson/ca_counties.json',
+    [GeoJsonDataset.CaAssemblyDistrict]: 'geojson/ca_assembly_legislative_districts.json',
+    [GeoJsonDataset.CaCongressionalLegislativeDistrict]: 'geojson/ca_congressional_legislative_districts.json',
+    [GeoJsonDataset.CaSenateLegislativeDistrict]: 'geojson/ca_senate_legislative_districts.json'
+}
--- index.ts
+++ index.ts
@@ -4,11 +4,9 @@ import * as fs from 'fs'
 import * as proj4 from 'proj4'
 import * as axios from 'axios'
 import * as PDFDocument from 'pdfkit'
-import { PROJECTIONS, Projection } from './projections'
+import { PROJECTIONS, GEOJSON_FILES, GeoJsonDataset } from './datasets'
 
 const token = process.env.MAPBOX_API_KEY
-const projection = 'PROJCS["WGS 84 / Pseudo-Mercator",GEOGCS["WGS 84",DATUM["WGS_1984",SPHEROID["WGS 84",6378137,298.257223563,AUTHORITY["EPSG","7030"]],AUTHORITY["EPSG","6326"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.0174532925199433,AUTHORITY["EPSG","9122"]],AXIS["Latitude",NORTH],AXIS["Longitude",EAST],AUTHORITY["EPSG","4326"]],PROJECTION["Mercator_1SP"],PARAMETER["central_meridian",0],PARAMETER["scale_factor",1],PARAMETER["false_easting",0],PARAMETER["false_northing",0],UNIT["metre",1,AUTHORITY["EPSG","9001"]],AXIS["X",EAST],AXIS["Y",NORTH],EXTENSION["PROJ4","+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs"],AUTHORITY["EPSG","3857"]]'
-
 
 const getCoordinates = async (address: string) => {
     const response = await axios.default.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=${token}`);
@@ -25,19 +23,32 @@ const run = async () => {
     const address = '1001 "I" Street, Sacramento, CA, 95814, USA'
     const coordinates = await getCoordinates(address)
 
-    const censusTract = getDacFeature(coordinates)
-    const loadServingEntity = getElectricLoadServingEntityFeature(coordinates)
-    const airDistrict = getCaAirDistrictFeature(coordinates)
-    const cleanCitiesCoalition = getCleanCitiesCoalitionFeature(coordinates)
-    const caCounty = getCaCountyFeature(coordinates)
+    const censusTract = getFeature(coordinates, GeoJsonDataset.DisadvantagedCommunity)
+    const loadServingEntity = getFeature(coordinates, GeoJsonDataset.ElectricLoadServingEntity)
+    const airDistrict = getFeature(coordinates, GeoJsonDataset.CaAirDistrict)
+    const cleanCitiesCoalition = getFeature(coordinates, GeoJsonDataset.CleanCitiesCoalition)
+    const caCounty = getFeature(coordinates, GeoJsonDataset.CaCounties)
+    const caAssemblyDistrict = getFeature(coordinates, GeoJsonDataset.CaAssemblyDistrict)
+    const caCongressionalDistrict = getFeature(coordinates, GeoJsonDataset.CaCongressionalLegislativeDistrict)
+    const caSenateDistrict = getFeature(coordinates, GeoJsonDataset.CaSenateLegislativeDistrict)
 
     console.log(censusTract)
     console.log(loadServingEntity)
     console.log(airDistrict)
     console.log(cleanCitiesCoalition)
     console.log(caCounty)
+    console.log(caAssemblyDistrict)
+    console.log(caCongressionalDistrict)
+    console.log(caSenateDistrict)
 
     generatePDF(address, censusTract.properties)
+}
+
+const getFeature = (coordinates: number[], dataset: GeoJsonDataset) => {
+    const projectedCoordinates = proj4.default(PROJECTIONS[dataset], coordinates)
+    const shapePoint = [projectedCoordinates[0], projectedCoordinates[1]];
+    const feature = findFeature(shapePoint, GEOJSON_FILES[dataset])
+    return feature
 }
 
 const findFeature = (shapePoint: number[], fileName: string) => {
@@ -51,47 +62,6 @@ const findFeature = (shapePoint: number[], fileName: string) => {
         }
     })
     return foundFeature
-}
-
-const getDacFeature = (coordinates: number[]) => {
-    const projectedCoordinates = proj4.default(PROJECTIONS[Projection.DisadvantagedCommunity], coordinates)
-    const shapePoint = [projectedCoordinates[0], projectedCoordinates[1]];
-
-    const dacFileName = 'geojson/dac.json'
-    const censusTract = findFeature(shapePoint, dacFileName)
-    return censusTract
-}
-
-const getElectricLoadServingEntityFeature = (coordinates: number[]) => {
-    const projectedCoordinates = proj4.default(PROJECTIONS[Projection.ElectricLoadServingEntity], coordinates)
-    const shapePoint = [projectedCoordinates[0], projectedCoordinates[1]];
-    const fileName = 'geojson/electric_load_serving_entities.json'
-    const loadEntity = findFeature(shapePoint, fileName)
-    return loadEntity
-}
-
-const getCaAirDistrictFeature = (coordinates: number[]) => {
-    const projectedCoordinates = proj4.default(PROJECTIONS[Projection.CaAirDistrict], coordinates)
-    const shapePoint = [projectedCoordinates[0], projectedCoordinates[1]];
-    const fileName = 'geojson/ca_air_districts.json'
-    const airDistrict = findFeature(shapePoint, fileName)
-    return airDistrict
-}
-
-const getCleanCitiesCoalitionFeature = (coordinates: number[]) => {
-    const projectedCoordinates = proj4.default(PROJECTIONS[Projection.CleanCitiesCoalition], coordinates)
-    const shapePoint = [projectedCoordinates[0], projectedCoordinates[1]];
-    const fileName = 'geojson/clean_cities_coalition.json'
-    const coalition = findFeature(shapePoint, fileName)
-    return coalition
-}
-
-const getCaCountyFeature = (coordinates: number[]) => {
-    const projectedCoordinates = proj4.default(PROJECTIONS[Projection.CaCounties], coordinates)
-    const shapePoint = [projectedCoordinates[0], projectedCoordinates[1]];
-    const fileName = 'geojson/ca_counties.json'
-    const coalition = findFeature(shapePoint, fileName)
-    return coalition
 }
 
 const generatePDF = (address: string, censusTractData: any) => {
